713231cfe34406d6e20d5fa948d07500f53ea1fd
Burger Ádám <burger.adam89@gmail.com>
2023-02-10 09:07:27 +0100

    RegistrationController fix for postman

--- src/main/java/com/codecool/liveMessenger/controller/RegistrationController.java
+++ src/main/java/com/codecool/liveMessenger/controller/RegistrationController.java
@@ -1,26 +1,22 @@
 package com.codecool.liveMessenger.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+@RestController
 @RequestMapping("/register")
 public class RegistrationController {
     private String name;
 
     @GetMapping
     public String displayRegistration(Model model) {
-        model.addAttribute("name", name != null ? name : "null");
-        return "register";
+        model.addAttribute("name", name != null ? name : null);
+        return name;
     }
 
     @PostMapping
     public String sendRegisterInfo(@RequestParam String register_name) {
         name = register_name;
-        return "redirect:";
+        return name;
     }
 }
