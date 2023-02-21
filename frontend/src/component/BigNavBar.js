2c5a9c8681b0c18b63d6bfba9001665605ab496a
david <makodavid1996@gmail.com>
2023-02-21 11:54:04 +0100

    WIP main page with react

---
+++ frontend/src/component/BigNavBar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const BigNavBar = () => {
+    return (
+        <div className='big-nav-bar'>
+
+        </div>
+    );
+};
+
+export default BigNavBar;
+
+//rsc az arrow function
+// msn színkód rgb(38,152,211)  vagy #2698D3
