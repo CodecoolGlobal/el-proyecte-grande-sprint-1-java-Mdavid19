cb6b8e451c446b51cb3119646c6cec3ff08b07dd
david <makodavid1996@gmail.com>
2023-02-23 11:11:28 +0100

    add hard coded friends for main page and some styel for it

--- frontend/src/component/FriendList.js
+++ frontend/src/component/FriendList.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import '../styles/MainPage.css'
+import BasicFriendList from "./BasicFriendList";
 
 const FriendList = () => {
     return (
         <div className={'friends-list'}>
+            <BasicFriendList/>
         </div>
     );
 };
