0f6c968ab58f5a3b657773beb557c1db170ac32b
Adam <burger.adam89@gmail.com>
2023-02-23 16:08:42 +0100

    Logo link fix

--- frontend/src/component/LandingPageLogo.js
+++ frontend/src/component/LandingPageLogo.js
@@ -1,13 +1,13 @@
-
-
-
 import React from 'react';
 import photo from '../images/ccLivelogo.png';
+import {Link} from "react-router-dom";
 
 const LandingPageLogo = () => {
     return (
-        <div className={["up-left","bouncy", "logo"].join(' ')}>
-            <img className={["up-left","logo"].join(" ")} src={photo} alt="CCLiveLogo"/>
+        <div className={["up-left", "bouncy", "logo"].join(' ')}>
+            <Link to={'/'}>
+                <img className={["up-left", "logo"].join(" ")} src={photo} alt="CCLiveLogo"/>
+            </Link>
         </div>
     );
 };
