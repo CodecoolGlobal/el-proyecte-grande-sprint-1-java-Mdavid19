47be6c7bc1afaf451eae4ac5df64f22a30f29021
david <makodavid1996@gmail.com>
2023-03-07 11:09:30 +0100

    add Message class and changing application properties db_user

---
+++ backend/src/main/java/com/codecool/liveMessenger/model/Message.java
@@ -0,0 +1,27 @@
+package com.codecool.liveMessenger.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@RequiredArgsConstructor
+@Data
+@Builder
+@Entity
+public class Message {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    private String content;
+    private Long senderId;
+    private Long receiverId;
+    private LocalDate date;
+}
