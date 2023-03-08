a30d55fa35dedeffc32428d7a6f9a810e41fb60b
WTTomi <103531873+WonkeTomi@users.noreply.github.com>
2023-03-08 10:48:10 +0100

    add fetch almost done

--- frontend/src/component/TextFieldForUserSettings.js
+++ frontend/src/component/TextFieldForUserSettings.js
@@ -2,6 +2,7 @@ import React, {useState} from "react";
 import {Button, createTheme, TextField, ThemeProvider} from "@mui/material";
 import '../styles/UserPage.css'
 import SaveIcon from '@mui/icons-material/Save';
+import axios from "../AxiosInstance";
 
 
 function SetUserInfo({labelForTextArea}) {
@@ -13,16 +14,32 @@ function SetUserInfo({labelForTextArea}) {
         },
     });
 
+    const url = "/user-profile"
+
+    const config = {
+        headers: {
+            'Content-type': 'application/json'
+        }
+    }
+
     const [content, setContent] = useState("");
 
-    const handleChange = (e) => {
-        debugger
-        console.log(e)
+    const handleChange = () => {
+        const data = JSON.stringify(content)
+        fetchSendUser(url, data, config)
+    }
+
+    const fetchSendUser = (url, body, config) => {
+        try {
+            axios.post(url, body, config)
+        } catch (error) {
+            console.warn('Some error', error)
+        }
     }
 
     return (<div>
             <ThemeProvider theme={theme}>
-                <TextField id="outlined-basic" label={labelForTextArea} variant="outlined" color='primary'/>
+                <TextField id="outlined-basic" label={labelForTextArea} variant="outlined" color='primary' onChange={(e)=> {setContent(e.target.value)}}/>
                 <br/>
                 <Button
                     variant="contained"
@@ -30,6 +47,7 @@ function SetUserInfo({labelForTextArea}) {
                     size="small"
                     sx={{margin: '7px'}}
                     startIcon={<SaveIcon sx={{color: 'blue'}}/>}
+                    type="submit"
                     onClick={handleChange}
                 >
                     Save
