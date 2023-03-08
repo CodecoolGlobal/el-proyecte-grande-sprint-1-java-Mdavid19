7d999508291b6655a85e38f53f0559b83eaf5142
WTTomi <103531873+WonkeTomi@users.noreply.github.com>
2023-03-08 13:56:22 +0100

    add route to change user name again

--- backend/src/main/java/com/codecool/liveMessenger/controller/UserProfileController.java
+++ backend/src/main/java/com/codecool/liveMessenger/controller/UserProfileController.java
@@ -1,15 +1,25 @@
 package com.codecool.liveMessenger.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.codecool.liveMessenger.service.UserService;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping
+@RequestMapping("/user-profile")
 public class UserProfileController {
 
-    @GetMapping("/user-profile")
+    UserService userService;
+
+    public UserProfileController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping
     public String userProfile() {
         return "Here you will be able to change your data";
+    }
+
+    @PostMapping
+    public void saveUserInfoChange(@RequestBody String data) {
+        userService.updateUserName(1L, data);
     }
 }
