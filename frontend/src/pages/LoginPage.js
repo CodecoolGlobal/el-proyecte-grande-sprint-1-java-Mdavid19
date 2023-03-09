b14e315645a2afe1236afbf1ab96ab90cacaf648
Burger Ádám <burger.adam89@gmail.com>
2023-03-09 14:38:32 +0100

    login with fetch, missing to send the user info to main page

--- frontend/src/pages/LoginPage.js
+++ frontend/src/pages/LoginPage.js
@@ -2,12 +2,16 @@ import {Button, FormControl, TextField} from "@mui/material";
 import LandingPageLogo from "../component/LandingPageLogo";
 import {useState} from "react";
 import axios from "../AxiosInstance";
+import {useNavigate} from 'react-router-dom';
 
 
 const LoginPage = () => {
     const [email, setEmail] = useState(null);
     const [password, setPassword] = useState(null);
-
+    const [state, setState] = useState(false);
+    const [loginStatus, setLoginStatus] = useState(false);
+    const [userId, setUserId] = useState(null);
+    const navigate = useNavigate();
 
     const url = "/login"
     const config = {
@@ -19,12 +23,19 @@ const LoginPage = () => {
         userEmail: email,
         userPassword: password
     }
+
     const handleChange = () => {
         sendUserInfoChanges(url, data, config)
+        if (state) {
+            navigate('/main', {replace: true})
+        }
     }
     const sendUserInfoChanges = (url, body, config) => {
         try {
-            axios.post(url, body, config)
+            axios.post(url, body, config).then((response) => {
+                const data = response.data
+                setState(data)
+            })
         } catch (error) {
             console.warn('Some error', error)
         }
@@ -44,7 +55,9 @@ const LoginPage = () => {
                     setPassword(event.target.value)
                 }}/>
                 <br/>
-                <Button variant="contained" size="large" type="submit" onClick={handleChange}>Login</Button>
+                <Button
+                    variant="contained" size="large" type="submit"
+                    onClick={handleChange}>Login</Button>
             </FormControl>
         </div>
     );
