255cbce86a4a8f4a4da1e3213efc1b949d1317d9
WTTomi <103531873+WonkeTomi@users.noreply.github.com>
2023-03-18 16:05:16 +0100

    add endpoint to uplad picture

--- backend/src/main/java/com/codecool/liveMessenger/controller/UserProfileController.java
+++ backend/src/main/java/com/codecool/liveMessenger/controller/UserProfileController.java
@@ -2,8 +2,12 @@ package com.codecool.liveMessenger.controller;
 
 import com.codecool.liveMessenger.model.ChatUser;
 import com.codecool.liveMessenger.service.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
 import java.util.Map;
 
 @RestController
@@ -27,5 +31,17 @@ public class UserProfileController {
     public void saveUserInfoChange(@RequestBody Map<String, String> data) {
 //      TODO  in session will be the user id just for testing we gave an id
         userService.updateUserInfo(1L, data);
+    }
+
+    @PostMapping("/upload")
+    public ResponseEntity<?> handleUploadFile(@RequestPart("file") MultipartFile file, @RequestPart("pictureType") String pictureType) {
+        String fileName = file.getOriginalFilename();
+
+        try {
+            file.transferTo(new File("/home/tamas/Projects/Advance/week_1/el-proyecte-grande-sprint-1-java-Mdavid19/user_pictures", fileName));
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+        return ResponseEntity.ok("File uploaded successfully");
     }
 }
