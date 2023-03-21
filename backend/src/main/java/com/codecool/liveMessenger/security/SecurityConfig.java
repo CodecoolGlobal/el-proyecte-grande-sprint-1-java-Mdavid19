4ccae011b66572e70e6730258474ff94a7878556
Lnikolett96 <privat.princess.chaos@gmail.com>
2023-03-21 14:01:20 +0100

    moving controller back to it s valid direction and implement JWTTokenVerifier Class

---
+++ backend/src/main/java/com/codecool/liveMessenger/security/SecurityConfig.java
@@ -0,0 +1,12 @@
+package com.codecool.liveMessenger.security;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfiguration {
+
+}
