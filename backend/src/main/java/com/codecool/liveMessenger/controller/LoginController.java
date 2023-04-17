7e030b6b21f01f6cccd1ff8492ccac4b80eefadc
WTTomi <103531873+WonkeTomi@users.noreply.github.com>
2023-04-17 10:57:48 +0200

    react token save WiP

--- backend/src/main/java/com/codecool/liveMessenger/controller/LoginController.java
+++ backend/src/main/java/com/codecool/liveMessenger/controller/LoginController.java
@@ -1,11 +1,16 @@
 package com.codecool.liveMessenger.controller;
 
+import com.codecool.liveMessenger.model.ChatUser;
+import com.codecool.liveMessenger.security.JwtAuthenticationFilter;
 import com.codecool.liveMessenger.security.request.AuthenticationRequest;
 import com.codecool.liveMessenger.security.response.AuthenticationResponse;
 import com.codecool.liveMessenger.service.AuthenticationService;
+import com.codecool.liveMessenger.service.JwtService;
 import com.codecool.liveMessenger.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,14 +18,26 @@ import java.util.Map;
 
 @CrossOrigin(origins = "http://localhost:3000")
 @RestController
-@RequestMapping("/login")
+@RequestMapping
 @RequiredArgsConstructor
 public class LoginController {
     @Autowired
     private AuthenticationService service;
+    @Autowired
+    private JwtService jwtService;
+    @Autowired
+    private UserService userService;
 
-    @PostMapping
+
+    @PostMapping("/login")
     public ResponseEntity<AuthenticationResponse> login(@RequestBody AuthenticationRequest request) {
     return ResponseEntity.ok(service.authenticate(request));
+    }
+
+    @GetMapping("/api/get-me")
+    public String getMe(@RequestHeader(HttpHeaders.AUTHORIZATION) String token) {
+        String jwtToken = token.substring(JwtAuthenticationFilter.JWT_TOKEN_START_INDEX);
+        String email = jwtService.extractUserName(jwtToken);
+        return userService.getUserByEmail(email).getChatUserName();
     }
 }
