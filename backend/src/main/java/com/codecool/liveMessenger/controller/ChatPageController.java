cdb23e5f6dbddb6b23408951cf1fd80c3a4665fa
david <makodavid1996@gmail.com>
2023-04-19 11:34:05 +0200

    add websocket backend implemetation

--- backend/src/main/java/com/codecool/liveMessenger/controller/ChatPageController.java
+++ backend/src/main/java/com/codecool/liveMessenger/controller/ChatPageController.java
@@ -1,6 +1,11 @@
 package com.codecool.liveMessenger.controller;
 
 
+import com.codecool.liveMessenger.model.Message;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,9 +14,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping
 public class ChatPageController {
 
-    @GetMapping("/user")
-    public String showMainPageAfterLogin(){
-        return "Hello this is the Main page";
-    }
+    @Autowired
+    private SimpMessagingTemplate simpMessagingTemplate;
 
+    @MessageMapping("/private-message")
+    public Message recievePrivateMessage(@Payload Message message){
+        simpMessagingTemplate.convertAndSendToUser(message.getReceiverId().toString(), "/private", message); // /user/receiver_id/private  this is where should the user listen
+        return message;
+    }
 }
