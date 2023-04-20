a0a5059cadfbbc7f784d3ee60d663f2e5a812677
david <makodavid1996@gmail.com>
2023-04-20 15:30:14 +0200

    websocket config on PageContent WIP

--- frontend/src/component/PageContent.js
+++ frontend/src/component/PageContent.js
@@ -2,10 +2,73 @@ import React, {useState} from 'react';
 import {Box, Button, InputAdornment, TextField} from "@mui/material";
 import BasicFriendList from "./BasicFriendList";
 import {useUser} from "../context/userProvider";
+import {over} from "stompjs"
+import SockJS from "sockjs-client"
+
+var stompClient = null
 
 const PageContent = () => {
-    const [content, setContent] = useState("");
+    const [privateChat, setPrivateChat] = useState(new Map());
+    const [tab, setTab] = useState("");
+    // Init userContextProvider
     const {user} = useUser();
+    const [messageData, setMessageData] = useState({
+        userId:user.id,                                             //Maybe it won't work
+        receiverId:"",
+        connected:false,
+        message:""
+    });
+
+
+
+    const connect = () => {
+        let Sock = new SockJS("http://localhost:3000/ws")
+        stompClient = over(Sock);
+        stompClient.connect({},onConnected,onError)
+    }
+
+    const onConnected = ()=>{
+        setMessageData({...messageData, "connected":true})
+        stompClient.subscribe('/user/' + messageData.userId + '/private', onPrivateMessageRecieved);
+    }
+
+    const onPrivateMessageRecieved = (payload)=>{
+        let payloadData = JSON.parse(payload.body);
+        if(privateChat.get(payloadData.senderId)){
+            privateChat.get(payloadData.senderId).push(payloadData)
+            setPrivateChat(new Map(privateChat))
+        }else{
+            let list = [];
+            list.push(payloadData);
+            privateChat.set(payloadData.senderId,list)
+            setPrivateChat(new Map(privateChat))
+        }
+    }
+
+    const onError = (err)=>{
+        console.log(err)
+    }
+
+    const handleMessage = (event)=>{
+        setMessageData({...messageData,"message":event.target})
+    }
+
+    const sendPrivateValue = ()=>{
+        if(stompClient){
+            var chatMessage = {
+                senderId:messageData.userId,
+                receiverId: tab,
+                message: messageData.message,
+            };
+        if(messageData.userId !== tab){
+            privateChat.get(tab).push(chatMessage)
+            setPrivateChat(new Map(privateChat));
+
+            }
+        stompClient.send("/app/private-message",{}, JSON.stringify(chatMessage));
+        setMessageData({...messageData, "message":""});
+        }
+    }
 
 
 
@@ -20,12 +83,12 @@ const PageContent = () => {
                 <TextField fullWidth InputProps={{
                     endAdornment: (
                         <InputAdornment position="end">
-                            <Button  variant="contained">Send</Button>
+                            <Button  variant="contained" onClick={sendPrivateValue}>Send</Button>
                         </InputAdornment>
                     ),
                 }} sx={{backgroundColor: '#FFFFFF'}} id="fullWidth"
-                           onChange={(e)=>{setContent(e.target.value)}}
-                           value={content}/>
+                           onChange={handleMessage}
+                           />
             </div>
         </div>
     );
