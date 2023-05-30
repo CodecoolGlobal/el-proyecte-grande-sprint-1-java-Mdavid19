824d6646fb312d1a65415905ccd73ddfc60e4435
WTTomi <103531873+WonkeTomi@users.noreply.github.com>
2023-05-30 14:18:32 +0200

    refactor user picture uplad in frontend

--- frontend/src/component/ImageUploader.js
+++ frontend/src/component/ImageUploader.js
@@ -1,29 +1,37 @@
 import React, {useState} from "react";
 import axios from "../AxiosInstance";
 import Cookies from "js-cookie";
+import {useUser} from "../context/userProvider";
 
 export default function ImageUploader({pictureText}) {
 
+    const {user} = useUser()
     const [file, setFile] = useState();
+    const token = Cookies.get('token')
 
     function handleChange(e) {
         setFile(e.target.files[0]);
     }
 
+    function makeFormData() {
+        const formData = new FormData();
+        formData.append('file', file);
+        formData.append('pictureType', pictureText)
+        formData.append('userId', user.id)
+        return formData
+    }
+
     function handleSubmit(event) {
         event.preventDefault();
         const url = '/chat-user-profile/upload';
-        const formData = new FormData();
-        const token = Cookies.get('token')
-        formData.append('file', file);
-        formData.append('pictureType', pictureText)
+
         const config = {
             headers: {
                 'content-type': 'multipart/form-data',
                 Authorization: `Bearer ${token}`
             },
         };
-        axios.post(url, formData, config).then((response) => {
+        axios.post(url, makeFormData(), config).then((response) => {
             console.log(response);
         });
     }
